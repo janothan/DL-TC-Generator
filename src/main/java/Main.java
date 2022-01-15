68618305ced8f78f4f9c13ec47d50a232753cf43
janothan <jan.portisch@gmail.com>
2022-01-15 14:50:45 +0100

    TC completed for people

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
 public class Main {
 
 
     public static void main(String[] args) throws Exception{
+        final String resultDir = "./result";
+        FileUtils.deleteDirectory(new File(resultDir));
         String queriesPath = Main.class.getResource("queries").getPath();
-        Generator generator = new Generator(queriesPath,"result");
+        Generator generator = new Generator(queriesPath,resultDir);
         generator.setSizes(new int[]{5, 10});
         generator.generateTestCases();
     }
